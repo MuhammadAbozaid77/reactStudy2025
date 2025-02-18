05f8e5fb613bf67415883b0216a304d67d2d561a
muhammadAbozaid <muhammadabozaid77@gmail.com>
2025-02-18 11:26:33 +0200

    last edited

--- src/main/project/Home.jsx
+++ src/main/project/Home.jsx
@@ -1,11 +1,11 @@
 import USearchParams from "../../react-study/react-router-dom/uSearchParams";
+import ReactQueryComponent from "../../react-study/reactQuery/ReactQueryComponent";
 
 export default function Home() {
-
-
   return (
     <div>
       <USearchParams />
+      <ReactQueryComponent />
     </div>
   );
 }
---
+++ src/react-study/reactQuery/ReactQueryComponent.jsx
@@ -0,0 +1,15 @@
+export default function ReactQueryComponent() {
+  return (
+    <div className="my-[50px] border p-5 rounded shadow">
+      <h1> ReactQueryComponent</h1>
+
+      <div>
+        <select name="" id="">
+          <option value="one">one</option>
+          <option value="two">two</option>
+          <option value="three">three</option>
+        </select>
+      </div>
+    </div>
+  );
+}
